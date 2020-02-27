9ead65ab1a60d220265a6f0cffc8a64cb89aa367
Dakota States <dakota.states@gmail.com>
2020-02-27 07:06:29 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,24 +1,13 @@
 class Project
-  attr_accessor :backer, :title
-  
+  attr_accessor :title, :backers
+
   def initialize(title)
     @title = title
     @backers = []
   end
-  
-  def backers
-    @backers
-  end
-  
+
   def add_backer(backer)
-    @backer = backer
     @backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
   end
-   
-   def backer=(backer)
-     @backer = backer
-     backer.back_project(self) unless backer.backed_projects.include?(self)
-   end
-
-
 end
